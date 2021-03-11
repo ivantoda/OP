cd93010fa789a2a9ae450beba9add50ee2fd08b7
Ivan Todoric <ivan.toda11@gmail.com>
2021-03-11 18:28:54 +0100

    Prvi commit

---
+++ vjezba2/vjezba2/vjezba2_1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void vraca(int *niz, int &a, int &b, int n) {
+    a = niz[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (a > niz[i])
+        {
+            a = niz[i];
+        }
+    }
+    b = niz[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (b < niz[i])
+        {
+            b = niz[i];
+        }
+    }
+}
+
+int main(){
+    int n, min, max;
+
+    cout << "unesi broj elemenata niza";
+    cin >> n;
+
+    int* niz = {};
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> niz[i];
+    }
+
+
+    vraca(niz, min, max, n);
+
+    cout << "najmanji elemnt je:" << min << "   najveci element je:" << max;
+}
